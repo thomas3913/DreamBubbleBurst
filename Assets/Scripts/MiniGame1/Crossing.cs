4f6a2c94bd51fd5268be56dd9da1f041b37647ba
thomas3913 <thomas_s_42@gmx.de>
2025-01-25 09:33:06 +0100

    Minigame1a update

---
+++ Assets/Scripts/MiniGame1/Crossing.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crossing : MonoBehaviour
+{
+
+    public bool left;
+    public bool right;
+
+    private List<string> directionList;
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        directionList = new List<string>();
+        
+        if(left){
+            directionList.Add("left");
+        }
+
+        if(right){
+            directionList.Add("right");
+        }
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.name == "Player"){
+            other.gameObject.GetComponent<Player_Cat>().SetPossibleDirections(directionList);
+        }
+    }
+}
